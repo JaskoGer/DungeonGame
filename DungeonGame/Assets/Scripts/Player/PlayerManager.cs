0d55b92a4638ebbddc050cbd11948d32758fd8b4
Toxsty <Tobias.Haubold@web.de>
2021-11-14 01:42:03 +0100

    Jump fixes, simple Respawn (Dungeon)

--- DungeonGame/Assets/Scripts/Player/PlayerManager.cs
+++ DungeonGame/Assets/Scripts/Player/PlayerManager.cs
@@ -15,8 +15,23 @@ public class PlayerManager : MonoBehaviour
         instance = this;
     }
 
-   #endregion
+    #endregion
 
-   public GameObject player;
+    public float lowestHeight = 0f;
+    public GameObject player;
+    public float StartPointx = 0f;
+    public float StartPointy = 0f;
+    public float StartPointz = 0f;
+    public Quaternion StartRotation = Quaternion.Euler(Vector3.zero);
+
+    private void Update()
+    {
+        if (player.transform.position.y < lowestHeight)
+        {
+            player.transform.Find("rotator").rotation = Quaternion.Euler(Vector3.zero);
+            player.transform.Find("RandyBeta").rotation = Quaternion.Euler(Vector3.zero);
+            player.transform.SetPositionAndRotation(new Vector3(StartPointx, StartPointy, StartPointz), StartRotation);
+        }
+    }
 
 }

c6d1a676986d7e3c430c0551e6e9b9efe50c42a2
Toxsty <Tobias.Haubold@web.de>
2021-11-23 18:59:46 +0100

    hallo

--- DungeonGame/Assets/Scripts/Enemy/EarlController.cs
+++ DungeonGame/Assets/Scripts/Enemy/EarlController.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]

38bcb86cbe929623959152fe3f06a51be04d8239
unknown <kacperbohlen@gmail.com>
2021-12-08 22:21:04 +0100

    funktionierender Shop und Inventar

--- DungeonGame/Assets/Prefabs/Inventory/Items/Equipment.cs
+++ DungeonGame/Assets/Prefabs/Inventory/Items/Equipment.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * @author Kacper Purtak
+ * version 1.0
+ * 03.12.2021
+ * Description: Vorlage für Ausruestungsobjekte, welche man dem Spieler ausruesten kann 
+ */
+
 [CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory/Equipment")]
 public class Equipment : NewItem
 {
-    public EquipmentSlot equipmentSlot;
+    public EquipmentSlot equipmentSlot;   //Funktion der Ausruestung
     public int armorModifier;
     public int damageModifier;
 
+    //wird aufgerugen, wenn die Ausruestung im Inventar gedrueckt wird
     public override void Use()
     {
         base.Use();
@@ -17,4 +25,5 @@ public class Equipment : NewItem
     }
 }
 
-public enum EquipmentSlot { Weapon, Armor, Potion }
+//Enum zum hinzuweisen der Funktion der Ausruestung
+public enum EquipmentSlot { Weapon, Armor}
--- DungeonGame/Assets/Prefabs/Inventory/Items/EquipmentManager.cs
+++ DungeonGame/Assets/Prefabs/Inventory/Items/EquipmentManager.cs
@@ -2,6 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * @author Kacper Purtak
+ * version 1.0
+ * 03.12.2021
+ * Description: Controller zum ausruesten von Ausruestung
+ */
+
 public class EquipmentManager : MonoBehaviour
 {
     #region Singleton
@@ -19,7 +26,7 @@ public class EquipmentManager : MonoBehaviour
 
     public delegate void OnEquipmentChanged(Equipment newItem, Equipment oldItem);
     public OnEquipmentChanged onEquipmentChanged;
-    public GameObject[] itemObj;
+    public GameObject[] itemObj;   //Array aus Gegenstaenden, die der Spieler ausruesten kann
 
     NewInventory inventory;
 
@@ -31,33 +38,42 @@ public class EquipmentManager : MonoBehaviour
         currentEquipment = new Equipment[numSlots];
     }
 
+    //ruestet ein neues Item aus
     public void Equip(Equipment newItem)
     {
-        int slotIndex = (int)newItem.equipmentSlot;
-
+        int slotIndex = (int)newItem.equipmentSlot;   //Slot-Platz, in den der Gegenstand passt
+        Debug.Log(slotIndex);
         Equipment oldItem = null;
 
+        //der zuletzt verwendete Gegenstand wird zurueck in das Inventar gelegt
         if (currentEquipment[slotIndex] != null)
         {
             oldItem = currentEquipment[slotIndex];
-            inventory.Add(oldItem);
+            
+			inventory.Add(oldItem);
+            itemObj[oldItem.itemID].SetActive(false);
+            Debug.Log("Altes Item wurde geadded");
         }
 
+        //ein Gegenstand wurde ausgeruestet, wodurch ein Delegate-Rueckruf ausgeloest wird
         if (onEquipmentChanged != null)
         {
             onEquipmentChanged.Invoke(newItem, oldItem);
+			Debug.Log("onEquipmentChanged wurde ausgeführt");
         }
-
+		
         currentEquipment[slotIndex] = newItem;
-        itemObj[slotIndex].SetActive(true);
+        itemObj[newItem.itemID].SetActive(true);
     }
 
+    //zieht die verwendete Item aus
     public void Unequip(int slotIndex)
     {
         if (currentEquipment[slotIndex] != null)
         {
             Equipment oldItem = currentEquipment[slotIndex];
             inventory.Add(oldItem);
+            itemObj[oldItem.itemID].SetActive(false);
 
             currentEquipment[slotIndex] = null;
 
@@ -68,6 +84,7 @@ public class EquipmentManager : MonoBehaviour
         }
     }
 
+    //zieht alle verwendeten Items aus
     public void UnequipAll()
     {
         for (int i = 0; i < currentEquipment.Length; i++)
@@ -77,6 +94,7 @@ public class EquipmentManager : MonoBehaviour
         }
     }
 
+    //prueft, ob alle vewendeten Ausruestungen ausgezogen werden
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.U))
